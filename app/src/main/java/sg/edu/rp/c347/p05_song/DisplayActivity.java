0ce36a81bc2c56f1f69816a8ef72af2f0fb72b65
Kiraow188 <17010368@myrp.edu.sg>
2019-05-21 12:52:01 +0800

    update

--- app/src/main/java/sg/edu/rp/c347/p05_song/DisplayActivity.java
+++ app/src/main/java/sg/edu/rp/c347/p05_song/DisplayActivity.java
@@ -2,12 +2,26 @@ package sg.edu.rp.c347.p05_song;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    ListView lv;
+    Button btn5stars;
+    ArrayAdapter aa;
+    ArrayList<Song> songs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        lv = findViewById(R.id.lv);
+        btn5stars = findViewById(R.id.btn5Stars);
+        
     }
 }
